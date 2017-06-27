687db9ca19dfde0a9de328df3145dffa083e408c
Dave Nicolette <davenicolette@gmail.com>
2017-06-27 13:22:26 -0700

    Added comments for rdoc to public methods in db.rb

--- lib/db.rb
+++ lib/db.rb
@@ -21,6 +21,7 @@ module Db
   	@conn = nil
   end
 
+  # Returns the substitution value for a given document set and key
   def substitution_text_for set_id, key
     auto_connect
     res = @substitutions
@@ -29,10 +30,12 @@ module Db
       .get(:value)
   end
 
+  # Returns a list of all substitution values for the default document set
   def default_substitutions
     substitutions_for DEFAULT
   end
 
+  # Returns a list of all substitution values for the specified document set name
   def substitutions_for set_name
     auto_connect
     @conn[:substitutions].select(:key, :value)
@@ -41,10 +44,12 @@ module Db
       .all
   end
 
+  # Returns a list of the default artifacts
   def default_artifacts
     artifacts_for DEFAULT
   end
 
+  # Returns a list of the artifacts for the specified document set name
   def artifacts_for set_name
     auto_connect
     @conn[:documents].select(:path)
